fe5d7270d3ffd0b75599fae0086936428b7b16f8
Kuba0517 <jakub.ratusz@gmail.com>
2024-12-28 16:46:48 +0100

    Add sidebar and cn function to merge tailwind classes

--- src/app/portfolios/page.tsx
+++ src/app/portfolios/page.tsx
@@ -6,10 +6,12 @@ export default async function PortfoliosPage() {
     const portfolios: PortfolioResponseDTO[] = await fetchPortfolios();
 
     return (
-        <div className="p-8">
-            <h1 className="text-2xl font-bold mb-4">Portfolios</h1>
-            <PortfolioList portfolios={portfolios}/>
-        </div>
+        <>
+            <div className="p-8">
+                <h1 className="text-2xl font-bold mb-4">Portfolios</h1>
+                <PortfolioList portfolios={portfolios}/>
+            </div>
+        </>
     );
 }
 
